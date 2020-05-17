914ebd19707725a8c9b47cd948324ce67c9bf23a
wzb <leimiliya@hotmail.com>
2020-05-17 23:06:27 +0800

    更新配置

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -11,17 +11,34 @@ module.exports = {
       description: '基于elementui的组件库文档',
     },
   },
-  // head: [
-  //   [
-  //     'link',
-  //     {
-  //       rel: 'stylesheet',
-  //       href: 'https://unpkg.com/element-ui/lib/theme-chalk/index.css',
-  //     },
-  //   ],
-  //   ['script', { src: 'https://unpkg.com/element-ui/lib/index.js' }],
-  // ],
+  head: [
+    ['link', { rel: 'icon', href: '/favicon.ico' }],
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    [
+      'meta',
+      { name: 'apple-mobile-web-app-status-bar-style', content: 'black' },
+    ],
+    ['link', { rel: 'apple-touch-icon', href: '/favicon.ico' }],
+    [
+      'link',
+      {
+        rel: 'stylesheet',
+        href: 'https://unpkg.com/element-ui/lib/theme-chalk/index.css',
+      },
+    ],
+    ['meta', { name: 'msapplication-TileImage', content: '/favicon.ico' }],
+    ['meta', { name: 'msapplication-TileColor', content: '#000000' }],
+  ],
   themeConfig: {
+    // 1.接受字符串，它设置了最后更新时间的label，例如：最后更新时间：2019年5月3日 21:51:53
+    lastUpdated: '最后更新时间',
+    // 2.设置true，开启最后更新时间
+    lastUpdated: true,
+    // 你的GitHub仓库，请正确填写
+    repo: 'https://github.com/idenet/w-element',
+    // 自定义仓库链接文字。
+    repoLabel: '仓库地址',
     nav: [{ text: '指南', link: '/pages/guide/' }],
     sidebar: {
       '/pages/guide/': [
@@ -37,6 +54,9 @@ module.exports = {
   cache: false,
   title: 'w-element',
   description: '基于elementui的组件库文档',
+  markdown: {
+    lineNumbers: true,
+  },
   plugins: [
     [
       'demo-code',
